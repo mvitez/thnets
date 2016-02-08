498cdab4f22fbc352b8f91597c2e0722772050b7
Marko Vitez <marko@vitez.it>
2016-02-08 23:30:50 +0100

    Remove warning

--- modules/SpatialConvolutionMM.c
+++ modules/SpatialConvolutionMM.c
@@ -72,7 +72,7 @@ static void nn_SpatialConvolutionMM_updateOutput_frame(THFloatTensor *input, THF
 	long nInputPlane, long inputWidth, long inputHeight,
 	long nOutputPlane, long outputWidth, long outputHeight)
 {
-	THFloatTensor *output2d;
+	THFloatTensor *output2d = 0;
 
 	if(finput)
 	{
