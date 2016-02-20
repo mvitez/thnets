30bc757ca08b208633f2eea64dd443f97d76d2bb
Marko Vitez <marko@vitez.it>
2016-02-20 10:02:38 +0100

    Normalize.c was missing

---
+++ modules/Normalize.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include "../thnets.h"
+
+THFloatTensor *nn_Normalize_updateOutput(struct module *module, THFloatTensor *input)
+{
+	THFloatTensor *output = module->output;
+	long i, j, n;
+	float sum;
+	float *idata, *odata;
+
+	THFloatTensor_resizeAs(output, input);
+	if(input->nDimension == 2)
+	{
+		for(i = 0; i < input->size[0]; i++)
+		{
+			sum = 0;
+			idata = input->storage->data + input->storageOffset + i * input->stride[0];
+			odata = output->storage->data + output->storageOffset + i * output->stride[0];
+			for(j = 0; j < input->size[1]; j++)
+				sum += idata[j] * idata[j];
+			sum = sqrtf(sum);
+			for(j = 0; j < input->size[1]; j++)
+				odata[j] = idata[j] / sum;
+		}
+	} else {
+		sum = 0;
+		idata = input->storage->data + input->storageOffset;
+		odata = output->storage->data + output->storageOffset;
+		n = THFloatTensor_nElement(input);
+		for(j = 0; j < n; j++)
+			sum += idata[j] * idata[j];
+		sum = sqrtf(sum);
+		for(j = 0; j < n; j++)
+			odata[j] = idata[j] / sum;
+	}
+	return output;
+}
